99117ee3d62f45f54cdf479bdf7fb7abb22786ef
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2024-11-13 20:23:46 +0200

    using template

---
+++ src/models/user.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, String
+from src.models.base import Base, BaseMixin
+
+
+class User(Base, BaseMixin):
+    """
+    Database model representing "users" table in the database.
+    UUID and table name are inherited from BaseMixin.
+    """
+
+    username = Column(String)
+    password = Column(String)
