400202c7ea68ccceef7163b0872aee25e61e073f
Rumyana <rummiev98@gmail.com>
2024-11-15 16:46:58 +0200

    request model updated

--- src/models/request.py
+++ src/models/request.py
@@ -10,9 +10,9 @@ class Request(Base, BaseMixin):
     Database model representing "user" table in the database.
     UUID and table name are inherited from BaseMixin.
     """
-    status = Column(Enum(RequestStatus), nullable=False)
+    status = Column(Enum(RequestStatus), nullable=False, default=RequestStatus.PENDING)
     request_date = Column(DateTime, default=func.now(), nullable=False)
-    response_date = Column(DateTime, nullable=False)
+    response_date = Column(DateTime, nullable=True)
     request_type = Column(Enum(RequestType), nullable=False)
 
     user_id = Column(UUID(as_uuid=True), ForeignKey("user.id"), nullable=False)
