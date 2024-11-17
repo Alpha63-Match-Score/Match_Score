dbafed6533a89ebcf3a5dc2ccc8e3b89854ed323
Aleksandar Vladimirov <aleksandar.vladimirov.a63@learn.telerikacademy.com>
2024-11-17 22:54:21 +0200

    - removed empty row

--- src/api/v1/routes.py
+++ src/api/v1/routes.py
@@ -12,5 +12,4 @@ api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(matches.router, prefix="/matches", tags=["matches"])
 api_router.include_router(tournaments.router, prefix="/tournaments", tags=["tournaments"])
 api_router.include_router(team.router, prefix="/teams", tags=["teams"])
-
 api_router.include_router(requests.router, prefix="/requests", tags=["requests"])
