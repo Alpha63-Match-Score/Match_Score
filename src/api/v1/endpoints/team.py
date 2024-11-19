c1902ef41b838f12a9865b022286db026d17a41e
Aleksandar Vladimirov <aleksandar.vladimirov.a63@learn.telerikacademy.com>
2024-11-19 22:00:46 +0200

    - added paginated and searchable get_teams - added TeamCreate as well - commented the UUID under TeamCreate Schema

--- src/api/v1/endpoints/team.py
+++ src/api/v1/endpoints/team.py
@@ -1,8 +1,21 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from src.api.deps import get_current_user, get_db
+from src.schemas.schemas import TeamCreate, UserResponse, TeamListResponse
+from src.crud import team as team_crud
+from src.utils.pagination import PaginationParams, get_pagination
 
 router = APIRouter()
 
 
 @router.get("/")
-def get_teams():
-    raise NotImplementedError
+def get_teams(db: Session = Depends(get_db),
+              pagination: PaginationParams = Depends(get_pagination),
+              search: str | None = None,
+              ):
+    return team_crud.get_teams(db, pagination, search)
+
+@router.post("/", response_model=TeamListResponse, status_code=201)
+def create_team(team: TeamCreate, db: Session = Depends(get_db), current_user: UserResponse = Depends(get_current_user)):
+    return team_crud.create_team(db, team, current_user)
