2b4c45c237d214ec3c405734fd7a31638f575125
Aleksandar Vladimirov <aleksandar.vladimirov.a63@learn.telerikacademy.com>
2024-11-21 21:27:46 +0200

    - team - win_percetage formatted in the stats - removed custom detailed message for team_exists and user_exists

--- src/api/v1/endpoints/team.py
+++ src/api/v1/endpoints/team.py
@@ -26,7 +26,7 @@ def get_teams(
     return team_crud.get_teams(db, pagination, search)
 
 
-@router.get("/{team_id}", response_model=TeamDetailedResponse, status_code=201)
+@router.get("/{team_id}", response_model=TeamDetailedResponse)
 def get_team(team_id: UUID, db: Session = Depends(get_db)):
     return team_crud.get_team(db, team_id)
 
@@ -40,7 +40,7 @@ def create_team(
     return team_crud.create_team(db, team, current_user)
 
 
-@router.put("/{team_id}", response_model=TeamListResponse, status_code=201)
+@router.put("/{team_id}", response_model=TeamListResponse)
 def update_team(
     team_id: UUID,
     team: TeamUpdate,
