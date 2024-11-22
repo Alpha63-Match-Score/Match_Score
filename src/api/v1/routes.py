4b9735c1609dd17a4d078d787558b21dc6377fb3
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2024-11-22 10:23:07 +0200

    change order of routes.py

--- src/api/v1/routes.py
+++ src/api/v1/routes.py
@@ -5,10 +5,10 @@ from fastapi import APIRouter
 api_router = APIRouter()
 
 api_router.include_router(users.router, prefix="/users", tags=["users"])
-api_router.include_router(matches.router, prefix="/matches", tags=["matches"])
+api_router.include_router(requests.router, prefix="/requests", tags=["requests"])
 api_router.include_router(
     tournaments.router, prefix="/tournaments", tags=["tournaments"]
 )
+api_router.include_router(matches.router, prefix="/matches", tags=["matches"])
 api_router.include_router(team.router, prefix="/teams", tags=["teams"])
-api_router.include_router(requests.router, prefix="/requests", tags=["requests"])
 api_router.include_router(player.router, prefix="/players", tags=["players"])
